c1aca7bfe234800f3c105bfaa319006c1a681b67
Alexandre Regis <alexandre.regis@usherbrooke.ca>
2015-09-15 00:15:50 -0400

    First iteration for the TAC module. In the tool->Options->Platform Action, the TAC is added. Some bugs still need to be fix and we missing the 2/3 of the code for now. Implementation of CAN com and understanding of Leaf table remaining.

---
+++ GUI/BioBotApp/BioBotApp/Controls/Option/Options/optionTAC.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace BioBotApp.Controls.Option.Options
+{
+    partial class optionTAC
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.bs_dtStepLeafBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.dsModuleStructureGUI = new BioBotApp.DataSets.dsModuleStructure2();
+            this.ta_StepLeaf = new BioBotApp.DataSets.dsModuleStructure2TableAdapters.taStepLeaf();
+            this.crudOptions1 = new BioBotApp.Controls.Utils.crudOptions();
+            this.pkidDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.descriptionDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bs_dtStepLeafBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dsModuleStructureGUI)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Controls.Add(this.crudOptions1);
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(330, 219);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "TAC";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.pkidDataGridViewTextBoxColumn,
+            this.descriptionDataGridViewTextBoxColumn});
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.DataSource = this.bs_dtStepLeafBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 69);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(321, 144);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // bs_dtStepLeafBindingSource
+            // 
+            this.bs_dtStepLeafBindingSource.DataMember = "dtStepLeaf";
+            this.bs_dtStepLeafBindingSource.DataSource = this.dsModuleStructureGUI;
+            // 
+            // dsModuleStructureGUI
+            // 
+            this.dsModuleStructureGUI.DataSetName = "dsModuleStructure2";
+            this.dsModuleStructureGUI.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // ta_StepLeaf
+            // 
+            this.ta_StepLeaf.ClearBeforeFill = true;
+            // 
+            // crudOptions1
+            // 
+            this.crudOptions1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.crudOptions1.ButtonRefreshVisible = false;
+            this.crudOptions1.LayoutLeftToRight = System.Windows.Forms.FlowDirection.RightToLeft;
+            this.crudOptions1.Location = new System.Drawing.Point(207, 12);
+            this.crudOptions1.MinimumSize = new System.Drawing.Size(37, 37);
+            this.crudOptions1.Name = "crudOptions1";
+            this.crudOptions1.Size = new System.Drawing.Size(118, 41);
+            this.crudOptions1.TabIndex = 0;
+            this.crudOptions1.AddClickHandler += new System.EventHandler(this.crudOptions_AddClickHandler);
+            this.crudOptions1.DeleteClickHandler += new System.EventHandler(this.crudOptions_DeleteClickHandler);
+            this.crudOptions1.ModifyClickHandler += new System.EventHandler(this.crudOptions_ModifyClickHandler);
+            // 
+            // pkidDataGridViewTextBoxColumn
+            // 
+            this.pkidDataGridViewTextBoxColumn.DataPropertyName = "pk_id";
+            this.pkidDataGridViewTextBoxColumn.HeaderText = "ID";
+            this.pkidDataGridViewTextBoxColumn.Name = "pkidDataGridViewTextBoxColumn";
+            this.pkidDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // descriptionDataGridViewTextBoxColumn
+            // 
+            this.descriptionDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.descriptionDataGridViewTextBoxColumn.DataPropertyName = "description";
+            this.descriptionDataGridViewTextBoxColumn.HeaderText = "Description";
+            this.descriptionDataGridViewTextBoxColumn.Name = "descriptionDataGridViewTextBoxColumn";
+            this.descriptionDataGridViewTextBoxColumn.ReadOnly = true;
+           
+            // 
+            // optionTAC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.groupBox1);
+            this.Name = "optionTAC";
+            this.Size = new System.Drawing.Size(330, 219);
+            this.Tag = "TAC";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bs_dtStepLeafBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dsModuleStructureGUI)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private Utils.crudOptions crudOptions1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource bs_dtStepLeafBindingSource;
+        private DataSets.dsModuleStructure2 dsModuleStructureGUI;
+        private DataSets.dsModuleStructure2TableAdapters.taStepLeaf ta_StepLeaf;
+        private System.Windows.Forms.DataGridViewTextBoxColumn pkidDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn descriptionDataGridViewTextBoxColumn;
+    }
+}
---
+++ GUI/BioBotApp/BioBotApp/Controls/Option/Options/optionTAC.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BioBotApp.Utils.Communication;
+
+namespace BioBotApp.Controls.Option.Options
+{
+    public partial class optionTAC : UserControl
+    {
+        public optionTAC()
+        {
+            InitializeComponent();
+        }
+        public optionTAC(DataSets.dsModuleStructure2 dsModuleStructure) : this()
+        {
+
+            dsModuleStructureGUI = dsModuleStructure;
+            bs_dtStepLeafBindingSource.DataSource = dsModuleStructureGUI;
+            if (this.dsModuleStructureGUI.dtActionType.Count == 0)
+            {
+                ta_StepLeaf.Fill(this.dsModuleStructureGUI.dtStepLeaf);
+            }
+        }
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        private void crudOptions_AddClickHandler(object sender, EventArgs e)
+        {
+            abstractDialog dialog = new abstractDialog("Action type", "Add");
+
+            namedInputTextBox description = new namedInputTextBox("Description");
+            dialog.addNamedInputTextBox(description);
+
+            dialog.ShowDialog();
+
+            if (dialog.DialogResult.Equals(DialogResult.OK))
+            {
+                DataSets.dsModuleStructure2.dtStepLeafRow row;
+
+                row = dsModuleStructureGUI.dtStepLeaf.NewdtStepLeafRow();
+                row.description = description.getInputTextValue();
+                dsModuleStructureGUI.dtStepLeaf.AdddtStepLeafRow(row);
+                updateRow(row);
+            }
+        }
+
+        private void crudOptions_DeleteClickHandler(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count == 0)
+            {
+                return;
+            }
+
+            DataSets.dsModuleStructure2.dtStepLeafRow row;
+            row = getSelectedRow();
+
+            DialogResult result = MessageBox.Show("Delete : " + row.description + " ?", "Delete action type ?", MessageBoxButtons.YesNo,
+                MessageBoxIcon.Warning);
+
+            if (result.Equals(DialogResult.No)
+                && row != null)
+            {
+                return;
+            }
+
+            row.Delete();
+            updateRow(row);
+        }
+
+        private void crudOptions_ModifyClickHandler(object sender, EventArgs e)
+        {
+            abstractDialog dialog = new abstractDialog("Action type", "Modify");
+            DataSets.dsModuleStructure2.dtStepLeafRow row = getSelectedRow();
+
+            if (row == null)
+            {
+                return;
+            }
+
+            namedInputTextBox description = new namedInputTextBox("Description", row.description);
+            dialog.addNamedInputTextBox(description);
+
+            dialog.ShowDialog();
+
+            if (dialog.DialogResult.Equals(DialogResult.OK))
+            {
+                row.description = description.getInputTextValue();
+                updateRow(row);
+            }
+        }
+        public DataSets.dsModuleStructure2.dtStepLeafRow getSelectedRow()
+        {
+            DataSets.dsModuleStructure2.dtStepLeafRow row;
+            DataRowView rowView = bs_dtStepLeafBindingSource.Current as DataRowView;
+            row = rowView.Row as DataSets.dsModuleStructure2.dtStepLeafRow;
+            return row;
+        }
+
+        public void updateRow(DataSets.dsModuleStructure2.dtStepLeafRow updateRow)
+        {
+            try
+            {
+                ta_StepLeaf.Update(updateRow);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Invalid action type, try again !",
+                    "Error !",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+                dsModuleStructureGUI.RejectChanges();
+            }
+        }
+    
+    }
+}
